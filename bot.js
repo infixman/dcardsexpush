960dfab8450c620bcff0ca9c80b1cade334fe37d
Esnchg <infixman@gmail.com>
2019-05-30 15:35:18 +0800

    bot say the same

--- bot.js
+++ bot.js
@@ -2,3 +2,9 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.BotToken;
 const bot = new TelegramBot(token, {polling: true});
 
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    if (msg.chat.text) {
+        bot.sendMessage(chatId, `${msg.from.username}: ${msg.chat.text}`);
+    };
+});
